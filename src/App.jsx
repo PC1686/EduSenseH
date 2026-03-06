a6ffcca4336d865562cc45badc4ccd6e14cb49a7
Prem Chougule <premchougule04@gmail.com>
2026-03-06 12:46:09 +0530

    integrate Tailwind CSS for responsive design

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div className='bg-blue-800 text-2xl text-white p-4'>App</div>
+    <div className='bg-blue-800 text-2xl text-white p-4'> App </div>
   )
 }
 
